61ff93acdbbedcb3ac4b297203e276d854fce598
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-12-26 06:58:16 +0000

    Swatch Config

---
+++ app/page.js
@@ -0,0 +1,25 @@
+"use client";
+import Header from "../components/Header";
+import Preview from "../components/Preview";
+import Configurator from "../components/Configurator";
+
+function Home() {
+  return (
+    <div
+      onContextMenu={(e) => e.preventDefault()}
+      className="mb-5 min-h-screen"
+    >
+      <Header />
+      <main className="mx-auto flex w-full max-w-7xl flex-col gap-2 px-2 sm:flex-row sm:gap-4">
+        <section className="w-full sm:w-3/5">
+          <Preview />
+        </section>
+        <aside className="w-full sm:w-2/5">
+          <Configurator />
+        </aside>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
--- src/components/Configurator.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Configurator() {
-  return <div>Configurator</div>;
-}
-
-export default Configurator;
